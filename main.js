5ee62da089a184862dce8feb37e9278d958e7e15
jonnasnovaes <jhonnata.novaes@lojainterativa.com>
2020-08-30 19:44:04 -0300

    Configuração padrão para buildar em (deb)

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const { resolve, basename } = require('path');
-const {app, Menu, Tray, MenuItem, dialog} = require('electron')
+const {app, Menu, Tray, MenuItem, dialog} = require('electron');
 const spawn = require('cross-spawn');
+//const spawnChild = require('child_process');
+
+//const root = 'cd ';
 
 const Store = require('electron-store');
 
@@ -16,69 +19,105 @@ let tray = null;
 
 function render() {
 
-  /*if(!tray.isDestroyed()){
+  if(!tray.isDestroyed()){
     tray.destroy();
-    tray = new Tray(resolve(__dirname, 'assets', 'tray-icon.png'));
-  }*/
+    tray = new Tray(resolve(__dirname, 'build/icons', '512x512.png'));
+  }
 
-const storeProjects = store.get('projects');
-const projects = storeProjects ? JSON.parse(storeProjects) : [];
+  const storeProjects = store.get('projects');
+  const projects = storeProjects ? JSON.parse(storeProjects) : [];
 
-const items = projects.map(project => ({
-  label: project.name,
-  submenu:[{
-    label: 'Abrir no VSCode',
-    click: () => spawn.sync('code', [project.path])
-  },
-  {
-    label: 'Remover',
-    click: () => {
-      store.set('projects', JSON.stringify(projects.filter(item => { item.path != projects.path } )));
-      render();
+  const items = projects.map(project => ({
+    label: project.name,
+    submenu:[
+      {
+        label: 'Abrir VSCode',
+        click: () => spawn.sync('code', [project.path])
+      },
+      {
+        label: 'Abrir Diretório',
+        click: () => spawn.sync('xdg-open', [project.path])
+      },
+      {
+        label: 'Abrir Terminal',
+        click: () => {
+          spawn.sync('x-terminal-emulator', [project.path])
+        } 
+      },
+      {
+        type: 'separator',
+      },
+      {
+        label: 'Remover',
+        click: () => {
+        
+          let itens = [];
+
+          projects.map((item) => {
+            if (item.path != project.path) {
+              itens.push(item);
+            }
+          });
+
+          store.set('projects', JSON.stringify(itens));
+          render();
+        }
+      }
+    ]
+  }));
+
+
+  //INSTANCIA CONFIGURANDO O MENU/ITENS
+  const contextMenu = Menu.buildFromTemplate([
+    ... items,
+    {
+      type: 'separator',
+    },
+    {
+      type: 'normal',
+      label: 'Sair',
+      role: 'quit',
+      enabled: true,
     }
-  }
-]
-}));
+  ]);
 
+  /**
+   * ADICIONA UM SEPARADOR APÓS O "ADICIONAR PROJETO"
+   */
+  contextMenu.insert(0, new MenuItem({
+    type: "separator"
+  }));
 
-//INSTANCIA CONFIGURANDO O MENU/ITENS
-const contextMenu = Menu.buildFromTemplate([
-  ... items,
-  {
-    type: 'separator',
-  },
-  {
-    type: 'normal',
-    label: 'Sair',
-    role: 'quit',
-    enabled: true,
-  }
-]);
+  contextMenu.insert(0, new MenuItem({
+    label: 'Adicionar Projeto..', click: () => { 
 
-contextMenu.insert(0, new MenuItem({ 
-  label: 'Adicionar Projeto..', click: () => { 
-    const [path] = dialog.showOpenDialogSync(null, { properties: ['openDirectory']});
-    const name = basename(path);
+      try {
+        const [path] = dialog.showOpenDialogSync(null, { properties: ['openDirectory']});
+        const name = basename(path);
     
-  
-    store.set('projects', JSON.stringify([ ... projects, {
-      path, 
-      name,
-    }]));
+        store.set('projects', JSON.stringify([ ... projects, {
+          path, 
+          name,
+        }]));
 
-      render();
+        render();
 
-   }
-   
-}));
+      } catch (error) {
+        console.log(error);
+      }
 
-tray.setToolTip('VSCode Projects');
-tray.setContextMenu(contextMenu);
+      
+    }   
+  }));
+
+  tray.setToolTip('VSCode Projects');
+  tray.setContextMenu(contextMenu);
 
 }
 
 app.on('ready', () => {
-    tray = new Tray(resolve(__dirname, 'assets', 'tray-icon.png'));
+    //tray = new Tray(resolve(__dirname, 'assets', 'tray-icon.png'));
+    tray = new Tray(resolve(__dirname, 'build/icons', '512x512.png'));
     render();
   }
 );
